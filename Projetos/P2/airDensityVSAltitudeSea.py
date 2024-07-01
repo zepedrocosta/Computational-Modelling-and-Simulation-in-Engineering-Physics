a8bff64df1baf077352793d676358bbfe7029cb2
José Costa <josepirescosta44.jc68@gmail.com>
2024-07-01 19:24:40 +0100

    refactor and add comparison

--- Projetos/P2/airDensityVSAltitudeSea.py
+++ Projetos/P2/airDensityVSAltitudeSea.py
@@ -79,8 +79,8 @@ def exponential_model(x, A, B):
     return A * np.exp(B * x)
 
 
-"""
-Plot the data and the exponential fit
+"""~
+Get the exponential fit
 
 Parameters
 ----------
@@ -88,8 +88,15 @@ altitude : numpy array
     The altitude values
 density : numpy array
     The density values
+    
+Returns
+-------
+fit_altitude : numpy array
+    The altitude values of the fit
+fit_density : numpy array
+    The density values of the fit
 """
-def plot_data_and_exponential_fit(altitude, density):
+def get_exponential_fit(altitude, density):
     altitude_norm = altitude / np.max(altitude)
 
     initial_guess = (1e-3, -1)
@@ -102,6 +109,46 @@ def plot_data_and_exponential_fit(altitude, density):
 
     fit_altitude = fit_altitude_norm * np.max(altitude)
 
+    return fit_altitude, fit_density
+
+
+"""
+Get the cubic spline fit
+
+Parameters
+----------
+altitude : numpy array
+    The altitude values
+density : numpy array
+    The density values
+
+Returns
+-------
+altitude_fine : numpy array
+    The altitude values of the fit
+density_fine : numpy array
+    The density values of the fit
+"""
+def get_cubic_spline_fit(altitude, density):
+    cs = CubicSpline(altitude, density, bc_type="natural")
+
+    altitude_fine = np.linspace(-1000, 150000, 500)
+    density_fine = cs(altitude_fine)
+
+    return altitude_fine, density_fine
+
+
+"""
+Plot the data and the exponential fit
+
+Parameters
+----------
+fit_altitude : numpy array
+    The altitude values of the fit
+fit_density : numpy array
+    The density values of the fit
+"""
+def plot_data_and_exponential_fit(altitude, density, fit_altitude, fit_density):
     plt.figure(figsize=(10, 8))
     plt.scatter(altitude, density, label="data")
     plt.plot(fit_altitude, fit_density, color="red", label="exp fit")
@@ -110,6 +157,7 @@ def plot_data_and_exponential_fit(altitude, density):
     plt.title("Air Density vs Altitude above sea level")
     plt.legend()
     plt.show()
+
 
 """
 Plot the data and the cubic spline fit
@@ -121,26 +169,34 @@ altitude : numpy array
 density : numpy array
     The density values
 """
-def plot_data_and_cubic_spline_fit(altitude, density):
-    cs = CubicSpline(altitude, density, bc_type='natural')
-
-    altitude_fine = np.linspace(-1000, 150000, 500)
-    density_fine = cs(altitude_fine)
-
+def plot_data_and_cubic_spline_fit(altitude, density, altitude_fine, density_fine):
     plt.figure(figsize=(10, 8))
-    plt.plot(altitude, density, 'bo', label='data')
+    plt.plot(altitude, density, "bo", label="data")
 
-    plt.plot(altitude_fine, density_fine, 'g-', label='cubic spline')
+    plt.plot(altitude_fine, density_fine, "g-", label="cubic spline")
 
     plt.xlabel("Altitude (m)")
     plt.ylabel("Density (kg m$^{-3}$)")
     plt.legend()
-    plt.title('Air density vs altitude above sea level - data and cubic spline')
+    plt.title("Air density vs altitude above sea level - data and cubic spline")
 
     plt.show()
 
+def plot_data_expontential_fit_cubic_spline_fit(altitude, density, fit_altitude, fit_density, altitude_fine, density_fine):
+    plt.figure(figsize=(10, 8))
+    plt.plot(altitude, density, "bo", label="data")
+    plt.plot(fit_altitude, fit_density, "r-", label="exp fit")
+    plt.plot(altitude_fine, density_fine, "g-", label="cubic spline")
+    plt.xlabel("Altitude (m)")
+    plt.ylabel("Density (kg m$^{-3}$)")
+    plt.legend()
+    plt.title("Air density vs altitude above sea level - data, exp fit and cubic spline")
+    plt.show()
 
 altitude, density = get_info_from_file(filename)
 plot_data(altitude, density)
-plot_data_and_exponential_fit(altitude, density)
-plot_data_and_cubic_spline_fit(altitude, density)
+fit_altitude, fit_density = get_exponential_fit(altitude, density)
+plot_data_and_exponential_fit(altitude, density, fit_altitude, fit_density)
+altitude_fine, density_fine = get_cubic_spline_fit(altitude, density)
+plot_data_and_cubic_spline_fit(altitude, density, altitude_fine, density_fine)
+plot_data_expontential_fit_cubic_spline_fit(altitude, density, fit_altitude, fit_density, altitude_fine, density_fine)
